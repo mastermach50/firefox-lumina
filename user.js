096a409267cb5734a54f4a1cc445fd033ff9d77d
Mathew Manoj (root) <64970593+mastermach50@users.noreply.github.com>
2024-10-26 01:30:18 +0530

    Redesigned firefox lumina

---
+++ user.js
@@ -0,0 +1,7 @@
+// Allow browser debugging
+user_pref("devtools.debugger.remote-enabled", true);
+// Open browser debugger without prompting the user
+user_pref("devtools.debugger.prompt-connection", false);
+
+// Allow UserStylesheets
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
